fb778cae9d75c4a37f9a694c0f9c0e79e39b300d
Ayesha1130 <ayeshaiqbal771@gmail.com>
2024-11-05 20:19:03 +0500

    first-commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <div className="bg-[#A29875] flex items-center w-full">
-      <div className=" flex items-center justify-between w-full max:w-[1486px]  h-[80px] px-4 py-4  mx-auto">
+      <div className=" flex items-center justify-between w-full max-w-[1486px]  h-[80px] px-4 py-4  mx-auto">
         <div className=" flex  items-center justify-start flex-shrink-0 ">
           <Image
             src={"/images/logo.png"}
@@ -16,13 +16,13 @@ const Navbar = () => {
         </div>
         {/* Search Bar Section */}
 
-        <div className="relative hidden rounded-lg md:flex items-center w-full h-12 lg:h-14 sm:w-[60%] lg:w-[40%] xl:w-[30%] mx-4 bg-neutral-100 ">
+        <div className="relative hidden rounded-lg md:flex items-center  h-12 lg:h-14 w-[60%] lg:w-[40%] xl:w-[30%] mx-4 bg-neutral-100 ">
           <input
             type="text"
             placeholder="Search for Gold Jewellery, Diamond Jewellery and more…"
             className=" w-full bg-transparent h-[40px] p-4 pl-10 text-sm  text-gray-600 outline-none "
           />
-          <div className="absolute right-3 top1/2 transform-translate-y-1/2">
+          <div className="absolute right-3 top-4/1 transform-translate-y-1/2">
             <Image
               src={"/images/searchicon.png"}
               alt="search icon"
